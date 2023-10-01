4d7568e884bb651a4fe91ecaf71f33da4bad6529
Eldar <eldar.kern@gmail.com>
2023-10-01 15:31:26 +0400

    created calculator

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-document.body.innerHTML = '<h1>Hello from JS</h1>'
+const calc = document.querySelector('.calc');
+const result = document.querySelector('#result');
+
+calc.addEventListener('click', function (event) {
+    if (!event.target.classList.contains('calc_btn')) return;
+
+    const value = event.target.innerText;
+
+
+
+    switch (value) {
+        case 'AC':
+            result.innerText = '';
+            break;
+
+        case '=':
+            result.innerText = eval(result.innerText).toFixed(1);
+            break;
+
+        default:
+            result.innerText += value;
+    }
+
+});
